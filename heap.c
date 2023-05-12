c664b3d6712c55d038e779b32ed82bab5b96b89c
Your Name <you@example.com>
2023-05-12 01:26:58 -0400

    new try

--- heap.c
+++ heap.c
@@ -65,7 +65,7 @@ void heap_pop(Heap* pq) {
       int posRight = 2 * pos + 2;
       int posMayor;
 
-      if (posLeft > pq->size) break;
+      if (posLeft >= pq->size) break;
 
       if (pq->heapArray[posLeft].priority > pq->heapArray[posRight].priority) {
          posMayor = posLeft;
